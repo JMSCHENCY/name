955e38305efb2797f48baa0d95a8b4080b84b0a8
James Chen <jmschency@gmail.com>
2018-12-19 19:57:27 +0800

    Add new file

---
+++ name.py
@@ -0,0 +1,15 @@
+my_name = "James Chen"
+my_age = str(33)
+print("My name is " + my_name + " and I'm " + my_age +" years old")
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello " + name + "!" + " You are " + age)
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a Plural noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
